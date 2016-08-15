b4a87e661c242532c50c7c396af1e286bf803b50
Aourin <TheTitan8706@gmail.com>
2016-08-14 23:41:27 -0400

    init(base): adds base unreal project

---
+++ Source/TankAndShield/TankAndShieldGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankAndShieldGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKANDSHIELD_API ATankAndShieldGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
