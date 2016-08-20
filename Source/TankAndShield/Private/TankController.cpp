bca7b6069d1e5ca6effb1712935ee6036d2ef07d
Aourin <TheTitan8706@gmail.com>
2016-08-20 14:40:32 -0400

    refactor(BP): updates controller gamemode and BPs with BP suffix

---
+++ Source/TankAndShield/Private/TankController.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAndShield.h"
+#include "TankController.h"
+
+
---
+++ Source/TankAndShield/Public/TankController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Public/Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKANDSHIELD_API ATankController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+};
